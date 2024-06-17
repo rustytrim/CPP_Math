4e8e19c759f496c62d374f913343cbbd1a5dd837
RT <rustytrim11@gmail.com>
2024-06-17 05:43:45 -0400

    fix: errors

--- src/main/include/vector/Vector3.h
+++ src/main/include/vector/Vector3.h
@@ -78,7 +78,7 @@ struct Vector3
 
 	T Magnitude() const
 	{
-		return std::sqrt((x * x) + (y * y) + (z * z);
+		return std::sqrt((x * x) + (y * y) + (z * z));
 	};
 
 	Vector3 &Normalize() const
@@ -91,12 +91,12 @@ struct Vector3
 	};
 
 	Vector3 Cross(const Vector3 &other) const {
-		return Vector3((y * other.z - z * other.y), (z * other.x - x * other.z), (x * other.y - y * other.x))
+		return Vector3((y * other.z - z * other.y), (z * other.x - x * other.z), (x * other.y - y * other.x));
 	};
 
 	T Dot(const Vector3 &other) const
 	{
-		return ((x * other.x) + (y + other.y) + (z + other.z);
+		return (x * other.x) + (y + other.y) + (z + other.z);
 	};
 
 	std::string ToString() const
--- src/test/main.cpp
+++ src/test/main.cpp
@@ -5,4 +5,12 @@
 int main()
 {
 	Vector3<units::degrees> v(0.0, 0.0, 0.0);
+	v.x = 90;
+	v.y = 45;
+	v.z = 25;
+
+	units::degrees yuh = 90;
+	units::radians hm = 1;
+
+	yuh = yuh + hm;
 }
